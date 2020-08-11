a5a434c7fa785213f67dca33595c2483a7ca7902
NIKI <niki801226@hotmail.com>
2020-08-11 17:47:30 +0800

    add covert2 to line

--- r1.py
+++ r1.py
@@ -6,6 +6,8 @@ def read_file(filename):
             lines.append(line.strip())
     return lines
 
+
+
 def convert(lines):
     new = []
     person = None  #還不知道是什麼
@@ -21,10 +23,45 @@ def convert(lines):
     return new
 
 
-def print_file(lines):
+def split_file(lines):
+    new = [[]]
+    
     for line in lines:
-        print(line)
 
+        new.append(line.strip('\t\n\r').split())
+
+         
+    return new
+
+def space_filter(lines):
+    new = []
+    Name = filter(None, lines)
+    for line in Name:
+        if len(line) >= 4: 
+            new.append(line)
+    
+    
+    print(new)
+    return new
+
+
+
+def convert2(lines):
+    new = []
+    person = None  #還不知道是什麼
+    
+    for line in lines:
+        #print(line)
+        if line[2] == '蓓萱ㄎㄎ':
+            person = '蓓萱ㄎㄎ'
+            new.append( line[0] + '---' + person + ':' + line[3])
+            continue
+        elif line[2] == '有容乃大':
+            person = '有容乃大'
+            new.append( line[0] + line[1] + '---' + person + ':' + line[3])
+            continue    
+        
+    return new
 
 def write_file(filename, lines):
     with open(filename, 'w', encoding = 'utf-8-sig') as f:
@@ -38,9 +75,12 @@ def main():
 
 
 
-    #lines = read_file('LINE_Chat.txt') 
-    #print_file(lines)
-    #write_file('Out_LINE_Chat.txt', lines)
+    lines = read_file('LINE_Chat.txt') 
+    lines = split_file(lines)
+    lines = space_filter(lines)
+    lines = convert2(lines)
+    #print(lines)
+    write_file('Out_LINE_Chat.txt', lines)
 
 
 main()
